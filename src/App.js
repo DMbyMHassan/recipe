f5013a4d540ee07ddc6f75c821db46d5c378326a
Mahmoud Hassan <129586375+DMbyMHassan@users.noreply.github.com>
2023-09-17 09:44:48 +0300

    bulding files and App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import RecipeForm from './components/RecipeForm';
+import AllRecipes from './pages/AllRecipes';
+import NewRecipe from './pages/NewRecipe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Website</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">All Recipes</Link>
+            </li>
+            <li>
+              <Link to="/new-recipe">New Recipe</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Switch>
+        <Route path="/" exact component={AllRecipes} />
+        <Route path="/new-recipe" component={NewRecipe} />
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
